964c035b4929a63925fbcb2dc7fa43c5720e1c8a
Triloworld <patryk@padus.pl>
2020-09-12 21:41:19 +0200

    Website template

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col min-h-screen bg-backgroundDark min-w-container">
+    <main class="flex flex-row relative flex-auto bg-white break-all xs:break-normal">
+      <nuxt />
+    </main>
+  </div>
+</template>
+
+<script>
+  export default {
+    head: {},
+  };
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="flex flex-col w-full m-auto">
+    IT'S WORK !
+  </div>  
+</template>
+
+<script>  
+  export default {};
+</script>
+
+<style lang="postcss" scoped>
+  
+</style>
